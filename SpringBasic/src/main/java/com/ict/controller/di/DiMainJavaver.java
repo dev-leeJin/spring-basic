2e68cef24dce235c7a97d338d1f772667e7e6cc4
dev-leeJin <ahgjark00@nate.com>
2022-03-24 12:26:59 +0900

    외부 환경 설정

---
+++ SpringBasic/src/main/java/com/ict/controller/di/DiMainJavaver.java
@@ -0,0 +1,21 @@
+package com.ict.controller.di;
+
+import com.ict.controller.di.classfile.Singer;
+import com.ict.controller.di.classfile.Stage;
+
+public class DiMainJavaver {
+
+	public static void main(String[] args) {
+		// 가수, 무대를 생성한 다음
+		Singer singer = new Singer(); // Singer이 없으면 실행이 안됌. (stage가 singer에 의존한다.) 
+		Stage stage = new Stage(singer);
+		
+		// 무대에 공연(perform)메서드를 호출해주세요.
+		stage.perform();
+		
+		// 그냥 가수가 노래하는것도 가능한지 테스트
+		singer.sing();
+
+	}
+
+}
