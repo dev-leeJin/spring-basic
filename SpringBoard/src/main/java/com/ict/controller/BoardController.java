a294587948a813fc97a2e3383845ab9d6fc88d94
dev-leeJin <ahgjark00@nate.com>
2022-04-08 18:27:19 +0900

    게시판

--- SpringBoard/src/main/java/com/ict/controller/BoardController.java
+++ SpringBoard/src/main/java/com/ict/controller/BoardController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ict.domain.BoardVO;
 import com.ict.mapper.BoardMapper;
@@ -41,10 +43,62 @@ public class BoardController {
 	// /boardDetail 주소를 get방식으로 선언해주세요.
 	// 주소 뒤에 ?bno=번호 형식으로 적힌 번호 글만 조회합니다.
 	// @PathValiable적용 방식으로 바꿔보세요. , boardList에서 제목클릭시 넘어오게해주세요.
-	@GetMapping("/boardDetail")
-	public String boardDetail(long bno, Model moder) {
+	// /boardDetail뒤에 /{bno}를 적고 변수 앞에 @PathVariable을 붙인다. 
+	@GetMapping("/boardDetail/{bno}") 
+	public String boardDetail(@PathVariable long bno, Model moder) {
 		BoardVO board = boardMapper.select(bno);
 		moder.addAttribute("board",board);
 		return "boardDetail";
+	}
+	
+	
+	// insert 페이지를 위한 form으로 연결되는 컨트롤러를 먼저 만들겠습니다.
+	// get방식으로 /boardInsert 주소를 접속시 form페이지로 연결됩니다.
+	// 폼 페이지의 이름은 boardForm.jsp입니다
+	@GetMapping("/boardInsert")
+	public String boardForm() {
+		return "boardForm";
+	}
+	
+	
+	// /boardInsert인데 post방식을 처리하는 메서드를 새로 만들어주세요.
+	// BoardVO를 입력받도록 해 주시면 실제로는 BoardVO의 멤버변수명으로 들어오는 자료를 입력받습니다.
+	// 입력받은 BoardVO를 토대로 mapper쪽의 insert 메서드를 실행해 주시고
+	// 리다이렉트는 return "redirect:/목적지주소" 형식으로 리턴구문을 작성하면 됩니다.
+	// boardList로 돌려보내주세요.
+	@PostMapping("/boardInsert")
+	public String boardInsert(BoardVO board) {
+		// 폼에서 날린 데이터 들어오는지 디버깅
+		log.info("들어온 데이터 디버깅 : " + board);
+		// insert 로직 실행
+		boardMapper.insert(board);
+		return "redirect:/boardList";
+	}
+	
+	// 글삭제 로직은 Post방식으로만 진행합니다.
+	// /boardDelete 주소로 처리하고
+	// bno를 받아서 해당 글을 삭제합니다.
+	// 글 삭제 버튼은 detail페이지 하단에 form으로 만들어서 bno를 hidden으로 전달하는
+	// submit버튼을 생성해서 처리하게 해주세요
+	// 삭제 수행 후 boardList로 리다이렉트해주세요.
+	@PostMapping("/boardDelete")
+	public String boardDelete(long bno) {
+		//삭제로직 실행
+		boardMapper.delete(bno);
+		//리턴으로 리스트페이지 복귀
+		return "redirect:boardList";
+	}
+	
+	// /boardUpdateForm를 Post방식으로 접속하는 form 연결 메서드를 만들겠습니다.
+	// update로직은 이미 데이터가 입력이 되어 있어야 합니다.
+	// 따라서 내가 수정하고자 하는 글의 정보를 VO로 받아온다음
+	// 폼 페이지에 포워딩해서 기입해놔야합니다.
+	// 폼페이지 이름은 boardUpdateForm.jsp입니다.
+	@PostMapping("/boardUpdateForm")
+	public String boardUpdateForm(long bno, Model model) {
+		BoardVO board = boardMapper.select(bno);
+		// 바인딩
+		model.addAttribute("board",board);
+		return "boardUpdateForm"; //${board};
 	}
 }
--- SpringBoard/src/main/webapp/WEB-INF/views/boardList.jsp
+++ SpringBoard/src/main/webapp/WEB-INF/views/boardList.jsp
@@ -4,10 +4,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <meta charset="EUC-KR">
 <title>Insert title here</title>
 </head>
 <body>
+	<div class="container">
 	<h1>게시물 목록</h1>
 	<table border="1">
 		<thead>
@@ -23,7 +25,7 @@
 			<c:forEach var="board" items="${boardList }">
 				<tr>
 					<th>${board.bno }</th>
-					<th><a href="#">${board.title }</th>
+					<th><a href="/boardDetail/${board.bno}">${board.title }</th>
 					<th>${board.writer }</th>
 					<th>${board.regdate }</th>
 					<th>${board.updatedate }</th>
@@ -31,5 +33,7 @@
 			</c:forEach>
 		</tbody>
 	</table>
+	<a href="/boardInsert" class="btn btn-success">글쓰기</a>
+	</div>
 </body>
 </html>
